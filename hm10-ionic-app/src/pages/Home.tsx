d21fbb26dc3d8100bf54cd4a8865f1af89fed02c
George <georgetech1984@gmail.com>
2023-08-11 23:12:52 +0900

    RadiCtrlOverride搭載のHM10に対して 前進、後退、停止命令を送信してラジコンを動かすことに成功

--- hm10-ionic-app/src/pages/Home.tsx
+++ hm10-ionic-app/src/pages/Home.tsx
@@ -1,40 +1,81 @@
-import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar, useIonToast } from '@ionic/react';
 import './Home.css';
 import { BLE } from '@awesome-cordova-plugins/ble'
 import { useCallback } from 'react';
 import { DeviceData } from '../models/DeviceData';
-import { format } from 'date-fns';
 
 const Home: React.FC = () => {
   /** HM-10のMAC ADDRESS */
-  const MAC_ADDRESS: string = 'DB457B9E-7D59-4EE2-42FA-B8EDC1DABF52';
+  //const MAC_ADDRESS: string = 'DB457B9E-7D59-4EE2-42FA-B8EDC1DABF52';
+  const MAC_ADDRESS: string = 'F52D143C-4F0F-0A28-4F9B-EA2B4F7F337A';
   const SERVICE_UUID: string = 'FFE0';
   const CHAR_UUID: string = 'FFE1';
+
+  const [presentToast] = useIonToast();
 
   const connect = useCallback(() => {
     BLE.connect(MAC_ADDRESS).subscribe(obj => {
       const str: string = JSON.stringify(obj);
       const data: DeviceData = JSON.parse(str);
       console.log(data);
+      presentToast({ message: `connected:${MAC_ADDRESS}`, duration: 1500 });
     });
-  }, [MAC_ADDRESS]);
+  }, [MAC_ADDRESS, presentToast]);
 
-  const send = useCallback(() => {
-    const msg: string = 'hello - ' + format(new Date(), 'yyyy/MM/dd');
+  const disconnect = useCallback(() => {
+    BLE.isConnected(MAC_ADDRESS).then(
+      () => {
+        BLE.disconnect(MAC_ADDRESS);
+        presentToast({ message: `disconnect:${MAC_ADDRESS}` });
+      });
+  }, [presentToast]);
 
-    const data: Uint8Array = new Uint8Array(msg.length);
-    for (let i = 0; i < data.length; i++) {
-      data[i] = msg.charCodeAt(i);
+
+
+  const send = useCallback(async (data: Uint8Array): Promise<void> => {
+    console.log('send:');
+    console.log(data);
+    try {
+      await BLE.write(MAC_ADDRESS, SERVICE_UUID, CHAR_UUID, data.buffer);
+    } catch (e) {
+      //tryで囲っておかないと以下のエラーを吐いて以上終了してしまう。
+      //Error Domain=CBATTErrorDomain Code=10 "The attribute could not be found." UserInfo={NSLocalizedDescription=The attribute could not be found.}
     }
-    console.log('send:' + msg);
-    BLE.write(MAC_ADDRESS, SERVICE_UUID, CHAR_UUID, data.buffer);
+  }, []);
 
-    BLE.read(MAC_ADDRESS, SERVICE_UUID, CHAR_UUID).then(rec => {
-      const data: Uint8Array = rec as Uint8Array;
-      const decoder: TextDecoder = new TextDecoder();
-      console.log('rec:' + decoder.decode(data));
+  const stop = useCallback(async (): Promise<void> => {
+    const data: Uint8Array = new Uint8Array(1);
+    data[0] = 0x00;
+    await send(data);
+  }, [send]);
+
+  const forward = useCallback(async (): Promise<void> => {
+    const data: Uint8Array = new Uint8Array(1);
+    data[0] = 0x01;
+    await send(data);
+  }, [send]);
+
+  const back = useCallback(async (): Promise<void> => {
+    const data: Uint8Array = new Uint8Array(1);
+    data[0] = 0x02;
+    await send(data);
+  }, [send]);
+
+  const waitSecond = useCallback(async (second: number): Promise<void> => {
+    return new Promise<void>((resolve) => {
+      setTimeout(resolve, second);
     });
   }, []);
+
+  const forward_back = useCallback(async (): Promise<void> => {
+    await forward();
+    await waitSecond(3000);
+    await stop();
+    await waitSecond(3000);
+    await back();
+    await waitSecond(3000);
+    await stop();
+  }, [forward, stop, waitSecond, back]);
 
   return (
     <IonPage>
@@ -44,13 +85,6 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Blank
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <IonGrid>
           <IonRow>
             <IonCol>
@@ -60,10 +94,37 @@ const Home: React.FC = () => {
             </IonCol>
 
             <IonCol>
-              <IonButton onClick={send}>
-                Send
+              <IonButton onClick={disconnect}>
+                Disconnect
               </IonButton>
             </IonCol>
+
+            <IonCol>
+              <IonButton onClick={stop}>
+                Stop
+              </IonButton>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonButton onClick={forward}>
+                Forward
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton onClick={back}>
+                Back
+              </IonButton>
+            </IonCol>
+
+            <IonCol>
+              <IonButton onClick={forward_back}>
+                Forward&Back
+              </IonButton>
+            </IonCol>
+
           </IonRow>
         </IonGrid>
       </IonContent>
